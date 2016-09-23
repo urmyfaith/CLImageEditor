d89ef6cadee13aeb58ae71073affd4d4cfab8591
zx <zuoxue@qq.com>
2016-09-23 14:30:24 +0800

    udpate

--- OptionalImageTools/CLEmoticonTool/CLEmoticonTool.m
+++ OptionalImageTools/CLEmoticonTool/CLEmoticonTool.m
@@ -13,14 +13,214 @@
 static NSString* const kCLEmoticonToolEmoticonPathKey = @"EmoticonPath";
 static NSString* const kCLEmoticonToolDeleteIconName = @"deleteIconAssetsName";
 
+#pragma mark - _CLEmoticonView
+
 @interface _CLEmoticonView : UIView
+
+//当前活动的 view
 + (void)setActiveEmoticonView:(_CLEmoticonView*)view;
+
+//获取内部imageView
 - (UIImageView*)imageView;
+
+//初始化方法: 创建图片,按钮,初始化缩放和角度,提供手势事件.
 - (id)initWithImage:(UIImage *)image tool:(CLEmoticonTool*)tool;
+
+//对图片实现缩放和旋转
 - (void)setScale:(CGFloat)scale;
 @end
 
+@implementation _CLEmoticonView
+{
+    UIImageView *_imageView;
+    UIButton *_deleteButton;
+    CLCircleView *_circleView;
 
+    CGFloat _scale;
+    CGFloat _arg;
+
+    CGPoint _initialPoint;
+    CGFloat _initialArg;
+    CGFloat _initialScale;
+}
+
++ (void)setActiveEmoticonView:(_CLEmoticonView*)view
+{
+    static _CLEmoticonView *activeView = nil;
+    if(view != activeView){
+        [activeView setAvtive:NO];
+        activeView = view;
+        [activeView setAvtive:YES];
+
+        [activeView.superview bringSubviewToFront:activeView];
+    }
+}
+
+- (id)initWithImage:(UIImage *)image tool:(CLEmoticonTool*)tool
+{
+    self = [super initWithFrame:CGRectMake(0, 0, image.size.width+32, image.size.height+32)];
+    if(self){
+        _imageView = [[UIImageView alloc] initWithImage:image];
+        _imageView.layer.borderColor = [[UIColor blackColor] CGColor];
+        _imageView.layer.cornerRadius = 3;
+        _imageView.center = self.center;
+        [self addSubview:_imageView];
+
+        _deleteButton = [UIButton buttonWithType:UIButtonTypeCustom];
+
+        [_deleteButton setImage:[tool imageForKey:kCLEmoticonToolDeleteIconName defaultImageName:@"btn_delete.png"] forState:UIControlStateNormal];
+        _deleteButton.frame = CGRectMake(0, 0, 32, 32);
+        _deleteButton.center = _imageView.frame.origin;
+        [_deleteButton addTarget:self action:@selector(pushedDeleteBtn:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:_deleteButton];
+
+        _circleView = [[CLCircleView alloc] initWithFrame:CGRectMake(0, 0, 32, 32)];
+        _circleView.center = CGPointMake(_imageView.width + _imageView.frame.origin.x, _imageView.height + _imageView.frame.origin.y);
+        _circleView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
+        _circleView.radius = 0.7;
+        _circleView.color = [UIColor whiteColor];
+        _circleView.borderColor = [UIColor blackColor];
+        _circleView.borderWidth = 5;
+        [self addSubview:_circleView];
+
+        _scale = 1;
+        _arg = 0;
+
+        [self initGestures];
+    }
+    return self;
+}
+
+- (void)initGestures
+{
+    _imageView.userInteractionEnabled = YES;
+    [_imageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewDidTap:)]];
+    [_imageView addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(viewDidPan:)]];
+    [_circleView addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(circleViewDidPan:)]];
+}
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    UIView* view= [super hitTest:point withEvent:event];
+    if(view==self){
+        return nil;
+    }
+    return view;
+}
+
+- (UIImageView*)imageView
+{
+    return _imageView;
+}
+
+//找出之前的活动视图,移除当前点击的视图
+- (void)pushedDeleteBtn:(id)sender
+{
+    _CLEmoticonView *nextTarget = nil;
+
+    const NSInteger index = [self.superview.subviews indexOfObject:self];
+
+    for(NSInteger i=index+1; i<self.superview.subviews.count; ++i){
+        UIView *view = [self.superview.subviews objectAtIndex:i];
+        if([view isKindOfClass:[_CLEmoticonView class]]){
+            nextTarget = (_CLEmoticonView*)view;
+            break;
+        }
+    }
+
+    if(nextTarget==nil){
+        for(NSInteger i=index-1; i>=0; --i){
+            UIView *view = [self.superview.subviews objectAtIndex:i];
+            if([view isKindOfClass:[_CLEmoticonView class]]){
+                nextTarget = (_CLEmoticonView*)view;
+                break;
+            }
+        }
+    }
+
+    [[self class] setActiveEmoticonView:nextTarget];
+    [self removeFromSuperview];
+}
+
+- (void)setAvtive:(BOOL)active
+{
+    _deleteButton.hidden = !active;
+    _circleView.hidden = !active;
+    _imageView.layer.borderWidth = (active) ? 1/_scale : 0;
+}
+
+//调整 iamgeview 的缩放
+// 自身的view 的 frame
+// imageview 的 center
+- (void)setScale:(CGFloat)scale
+{
+    _scale = scale;
+
+    self.transform = CGAffineTransformIdentity;
+
+    _imageView.transform = CGAffineTransformMakeScale(_scale, _scale);
+
+    CGRect rct = self.frame;
+    rct.origin.x += (rct.size.width - (_imageView.width + 32)) / 2;
+    rct.origin.y += (rct.size.height - (_imageView.height + 32)) / 2;
+    rct.size.width  = _imageView.width + 32;
+    rct.size.height = _imageView.height + 32;
+    self.frame = rct;
+
+    _imageView.center = CGPointMake(rct.size.width/2, rct.size.height/2);
+
+    self.transform = CGAffineTransformMakeRotation(_arg);
+
+    _imageView.layer.borderWidth = 1/_scale;
+    _imageView.layer.cornerRadius = 3/_scale;
+}
+
+- (void)viewDidTap:(UITapGestureRecognizer*)sender
+{
+    [[self class] setActiveEmoticonView:self];
+}
+
+- (void)viewDidPan:(UIPanGestureRecognizer*)sender
+{
+    [[self class] setActiveEmoticonView:self];
+
+    CGPoint p = [sender translationInView:self.superview];
+
+    if(sender.state == UIGestureRecognizerStateBegan){
+        _initialPoint = self.center;
+    }
+    self.center = CGPointMake(_initialPoint.x + p.x, _initialPoint.y + p.y);
+}
+
+- (void)circleViewDidPan:(UIPanGestureRecognizer*)sender
+{
+    CGPoint p = [sender translationInView:self.superview];
+
+    static CGFloat tmpR = 1;
+    static CGFloat tmpA = 0;
+    if(sender.state == UIGestureRecognizerStateBegan){
+        _initialPoint = [self.superview convertPoint:_circleView.center fromView:_circleView.superview];
+
+        CGPoint p = CGPointMake(_initialPoint.x - self.center.x, _initialPoint.y - self.center.y);
+        tmpR = sqrt(p.x*p.x + p.y*p.y);
+        tmpA = atan2(p.y, p.x);
+
+        _initialArg = _arg;
+        _initialScale = _scale;
+    }
+
+    p = CGPointMake(_initialPoint.x + p.x - self.center.x, _initialPoint.y + p.y - self.center.y);
+    CGFloat R = sqrt(p.x*p.x + p.y*p.y);
+    CGFloat arg = atan2(p.y, p.x);
+
+    _arg   = _initialArg + arg - tmpA;
+    [self setScale:MAX(_initialScale * R / tmpR, 0.2)];
+}
+
+@end
+
+
+#pragma mark - CLEmoticonTool
 
 @implementation CLEmoticonTool
 {
@@ -194,187 +394,3 @@ static NSString* const kCLEmoticonToolDeleteIconName = @"deleteIconAssetsName";
 @end
 
 
-@implementation _CLEmoticonView
-{
-    UIImageView *_imageView;
-    UIButton *_deleteButton;
-    CLCircleView *_circleView;
-    
-    CGFloat _scale;
-    CGFloat _arg;
-    
-    CGPoint _initialPoint;
-    CGFloat _initialArg;
-    CGFloat _initialScale;
-}
-
-+ (void)setActiveEmoticonView:(_CLEmoticonView*)view
-{
-    static _CLEmoticonView *activeView = nil;
-    if(view != activeView){
-        [activeView setAvtive:NO];
-        activeView = view;
-        [activeView setAvtive:YES];
-        
-        [activeView.superview bringSubviewToFront:activeView];
-    }
-}
-
-- (id)initWithImage:(UIImage *)image tool:(CLEmoticonTool*)tool
-{
-    self = [super initWithFrame:CGRectMake(0, 0, image.size.width+32, image.size.height+32)];
-    if(self){
-        _imageView = [[UIImageView alloc] initWithImage:image];
-        _imageView.layer.borderColor = [[UIColor blackColor] CGColor];
-        _imageView.layer.cornerRadius = 3;
-        _imageView.center = self.center;
-        [self addSubview:_imageView];
-        
-        _deleteButton = [UIButton buttonWithType:UIButtonTypeCustom];
-		
-        [_deleteButton setImage:[tool imageForKey:kCLEmoticonToolDeleteIconName defaultImageName:@"btn_delete.png"] forState:UIControlStateNormal];
-        _deleteButton.frame = CGRectMake(0, 0, 32, 32);
-        _deleteButton.center = _imageView.frame.origin;
-        [_deleteButton addTarget:self action:@selector(pushedDeleteBtn:) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:_deleteButton];
-        
-        _circleView = [[CLCircleView alloc] initWithFrame:CGRectMake(0, 0, 32, 32)];
-        _circleView.center = CGPointMake(_imageView.width + _imageView.frame.origin.x, _imageView.height + _imageView.frame.origin.y);
-        _circleView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
-        _circleView.radius = 0.7;
-        _circleView.color = [UIColor whiteColor];
-        _circleView.borderColor = [UIColor blackColor];
-        _circleView.borderWidth = 5;
-        [self addSubview:_circleView];
-        
-        _scale = 1;
-        _arg = 0;
-        
-        [self initGestures];
-    }
-    return self;
-}
-
-- (void)initGestures
-{
-    _imageView.userInteractionEnabled = YES;
-    [_imageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewDidTap:)]];
-    [_imageView addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(viewDidPan:)]];
-    [_circleView addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(circleViewDidPan:)]];
-}
-
-- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
-{
-    UIView* view= [super hitTest:point withEvent:event];
-    if(view==self){
-        return nil;
-    }
-    return view;
-}
-
-- (UIImageView*)imageView
-{
-    return _imageView;
-}
-
-- (void)pushedDeleteBtn:(id)sender
-{
-    _CLEmoticonView *nextTarget = nil;
-    
-    const NSInteger index = [self.superview.subviews indexOfObject:self];
-    
-    for(NSInteger i=index+1; i<self.superview.subviews.count; ++i){
-        UIView *view = [self.superview.subviews objectAtIndex:i];
-        if([view isKindOfClass:[_CLEmoticonView class]]){
-            nextTarget = (_CLEmoticonView*)view;
-            break;
-        }
-    }
-    
-    if(nextTarget==nil){
-        for(NSInteger i=index-1; i>=0; --i){
-            UIView *view = [self.superview.subviews objectAtIndex:i];
-            if([view isKindOfClass:[_CLEmoticonView class]]){
-                nextTarget = (_CLEmoticonView*)view;
-                break;
-            }
-        }
-    }
-    
-    [[self class] setActiveEmoticonView:nextTarget];
-    [self removeFromSuperview];
-}
-
-- (void)setAvtive:(BOOL)active
-{
-    _deleteButton.hidden = !active;
-    _circleView.hidden = !active;
-    _imageView.layer.borderWidth = (active) ? 1/_scale : 0;
-}
-
-- (void)setScale:(CGFloat)scale
-{
-    _scale = scale;
-    
-    self.transform = CGAffineTransformIdentity;
-    
-    _imageView.transform = CGAffineTransformMakeScale(_scale, _scale);
-    
-    CGRect rct = self.frame;
-    rct.origin.x += (rct.size.width - (_imageView.width + 32)) / 2;
-    rct.origin.y += (rct.size.height - (_imageView.height + 32)) / 2;
-    rct.size.width  = _imageView.width + 32;
-    rct.size.height = _imageView.height + 32;
-    self.frame = rct;
-    
-    _imageView.center = CGPointMake(rct.size.width/2, rct.size.height/2);
-    
-    self.transform = CGAffineTransformMakeRotation(_arg);
-    
-    _imageView.layer.borderWidth = 1/_scale;
-    _imageView.layer.cornerRadius = 3/_scale;
-}
-
-- (void)viewDidTap:(UITapGestureRecognizer*)sender
-{
-    [[self class] setActiveEmoticonView:self];
-}
-
-- (void)viewDidPan:(UIPanGestureRecognizer*)sender
-{
-    [[self class] setActiveEmoticonView:self];
-    
-    CGPoint p = [sender translationInView:self.superview];
-    
-    if(sender.state == UIGestureRecognizerStateBegan){
-        _initialPoint = self.center;
-    }
-    self.center = CGPointMake(_initialPoint.x + p.x, _initialPoint.y + p.y);
-}
-
-- (void)circleViewDidPan:(UIPanGestureRecognizer*)sender
-{
-    CGPoint p = [sender translationInView:self.superview];
-    
-    static CGFloat tmpR = 1;
-    static CGFloat tmpA = 0;
-    if(sender.state == UIGestureRecognizerStateBegan){
-        _initialPoint = [self.superview convertPoint:_circleView.center fromView:_circleView.superview];
-        
-        CGPoint p = CGPointMake(_initialPoint.x - self.center.x, _initialPoint.y - self.center.y);
-        tmpR = sqrt(p.x*p.x + p.y*p.y);
-        tmpA = atan2(p.y, p.x);
-        
-        _initialArg = _arg;
-        _initialScale = _scale;
-    }
-    
-    p = CGPointMake(_initialPoint.x + p.x - self.center.x, _initialPoint.y + p.y - self.center.y);
-    CGFloat R = sqrt(p.x*p.x + p.y*p.y);
-    CGFloat arg = atan2(p.y, p.x);
-    
-    _arg   = _initialArg + arg - tmpA;
-    [self setScale:MAX(_initialScale * R / tmpR, 0.2)];
-}
-
-@end
